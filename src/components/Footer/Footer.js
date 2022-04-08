bacaf2960d921923efaf32582743750bbe60995c
madan <madan9>
2022-04-08 13:18:23 +0545

    makeing footer more responive and added contacts on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Row, Typography } from "antd";
+import MailImg from "../../images/email.svg";
 import "./Footer.scss";
 
 const Footer = () => {
@@ -10,6 +12,34 @@ const Footer = () => {
         <small>
           <b style={{ color: "#fff" }}>designed and created by madan ghimire</b>
         </small>
+        <div style={{ marginLeft: "1.5rem" }}>
+          <a href="mailto:mail2madanghimire@gmail.com">
+            <Typography
+              style={{
+                color: "#fff",
+                fontSize: "15px",
+                marginBottom: "15px",
+                display: "flex",
+                alignContent: "center",
+                alignItems: "center",
+                cursor: "pointer",
+              }}
+            >
+              <img
+                style={{
+                  color: "#fff",
+                  marginRight: "2rem",
+                  marginBottom: "-2px",
+                }}
+                src={MailImg}
+                width={15}
+                height={15}
+                alt="img"
+              />{" "}
+              mail2madanghimire
+            </Typography>
+          </a>
+        </div>
       </div>
     </div>
   );
